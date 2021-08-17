d2c4870a7039560614be992d1b92604322e7adfe
galuropek <Z322200119i>
2021-08-17 22:43:33 +0300

    clear commented code from migration

--- db/migrate/20210817184636_create_tests_users_join_table.rb
+++ db/migrate/20210817184636_create_tests_users_join_table.rb
@@ -1,11 +1,5 @@
 class CreateTestsUsersJoinTable < ActiveRecord::Migration
   def change
     create_join_table :users, :tests
-    # create_table :testsusers do |t|
-    #   t.integer :test_id
-    #   t.integer :user_id
-    #
-    #   t.timestamps null: false
-    # end
   end
 end
