92112e1be4ccf3099e3dbee181f5970d8233d125
galuropek <Z322200119i>
2021-08-17 22:25:15 +0300

    add class method fir searching tests titles by category name

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
 class Test < ActiveRecord::Base
+  has_and_belongs_to_many :users
+
+  def self.titles_by_category_name(category_name)
+    Test.joins('JOIN categories ON categories.id = tests.category_id')
+      .where('categories.title = ?', category_name)
+      .order(title: :desc)
+      .pluck(:title)
+  end
 end
