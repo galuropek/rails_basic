c13b6b2f41aa7d7faa3db8afc5810c25c303c7f8
galuropek <Z322200119i>
2021-08-18 22:50:54 +0300

    add associations to User and Test models

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,7 @@ class Test < ActiveRecord::Base
   has_many :questions
   has_and_belongs_to_many :users
   belongs_to :category
+  belongs_to :user
 
   def self.titles_by_category_name(category_name)
     Test.joins('JOIN categories ON categories.id = tests.category_id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :tests
+  has_many :tests
 
   def tests_by_level(level)
     self.tests.where(level: level)
