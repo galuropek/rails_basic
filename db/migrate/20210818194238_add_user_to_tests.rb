6ce3e414345cf924afebb635546bd4084cc68c30
galuropek <Z322200119i>
2021-08-18 22:49:42 +0300

    remove extra index

--- db/migrate/20210818194238_add_user_to_tests.rb
+++ db/migrate/20210818194238_add_user_to_tests.rb
@@ -1,5 +1,5 @@
 class AddUserToTests < ActiveRecord::Migration
   def change
-    add_reference :tests, :user, index: true, foreign_key: true
+    add_reference :tests, :user, foreign_key: true
   end
 end
