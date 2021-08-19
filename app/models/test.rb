815bd3f67e1851893a4dad6c858b5dd0f674bf5a
galuropek <Z322200119i>
2021-08-19 14:24:29 +0300

    fix incorrect associations in Test and User models

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,8 @@ class Test < ActiveRecord::Base
   has_many :questions
   has_and_belongs_to_many :users
   belongs_to :category
-  belongs_to :user
+  belongs_to :owner, foreign_key: "user_id", class_name: 'User'
+  # belongs_to :user
 
   def self.titles_by_category_name(category_name)
     Test.joins('JOIN categories ON categories.id = tests.category_id')
