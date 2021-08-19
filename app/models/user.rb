c069d1c3774e91c06f4d7eff03125b485387d927
galuropek <Z322200119i>
2021-08-19 22:58:02 +0300

    fix aliases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :tests
-  has_many :owned_tests, class_name: 'Test'
+  has_many :created_tests, class_name: 'Test'
 
   def tests_by_level(level)
     self.tests.where(level: level)
