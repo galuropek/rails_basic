1439d441326190819af72d76c315cc973a8df01e
galuropek <Z322200119i>
2021-08-26 10:44:03 +0300

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :tests
   has_many :created_tests, class_name: 'Test'
 
+  validates :email, presence: true
+
   def tests_by_level(level)
     self.tests.where(level: level)
   end
