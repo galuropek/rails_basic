98ae1045442de264cd57ff0733e4d022f09daf60
galuropek <Z322200119i>
2021-08-26 10:43:58 +0300

    add validates

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,10 @@ class Test < ActiveRecord::Base
   belongs_to :category
   belongs_to :creator, foreign_key: "user_id", class_name: 'User'
 
+  validates :title, presence: true,
+                    uniqueness: {scope: :level}
+  validates :level, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
   scope :by_level, -> (level) { where(level: level) }
   scope :easy, -> { by_level(0..1) }
   scope :medium, -> { by_level(2..4) }
@@ -13,6 +17,6 @@ class Test < ActiveRecord::Base
     joins('JOIN categories ON categories.id = tests.category_id')
       .where('categories.title = ?', category_name)
       .order(title: :desc)
-      .pluck(:title)    
+      .pluck(:title)
   }
 end
