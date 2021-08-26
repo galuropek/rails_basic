8fb67f0ca4c54a4d61913f31dcfd520d25dfe5b7
galuropek <Z322200119i>
2021-08-26 10:44:25 +0300

    add validates

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,15 @@
 class Question < ActiveRecord::Base
   belongs_to :test
   has_many :answers
+
+  validates :body, presence: true
+  validate :validate_answers_count
+
+  private
+
+  def validate_answers_count
+    min, max = [1, 4]
+    errors.add(:answers_count, "expected count between %s and %s" % [min, max]) unless
+      answers.size.between?(min, max)
+  end
 end
