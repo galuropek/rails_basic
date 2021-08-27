d0999f90bfa45ffa3b7c531474d9947f27c1c7d4
galuropek <Z322200119i>
2021-08-27 10:09:41 +0300

    transfer validate_answers_count validation to Answer model

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,13 +3,4 @@ class Question < ActiveRecord::Base
   has_many :answers
 
   validates :body, presence: true
-  validate :validate_answers_count
-
-  private
-
-  def validate_answers_count
-    min, max = [1, 4]
-    errors.add(:answers_count, "expected count between %s and %s" % [min, max]) unless
-      answers.size.between?(min, max)
-  end
 end
