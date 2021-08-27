9c5ad25c09792800764e62137b21f46983d4bc59
galuropek <Z322200119i>
2021-08-27 10:08:59 +0300

    add validate_answers_count

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,17 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   validates :body, presence: true
+  validate :validate_answers_count, on: :create
+
+  MAX_ANSWERS_COUNT = 4
 
   scope :correct_answers, -> { where(correct: true) }
+
+  private
+
+  def validate_answers_count
+    if question && question.answers.size >= MAX_ANSWERS_COUNT
+      errors.add(:answers_count, "expected: question contains <= #{MAX_ANSWERS_COUNT} answers")
+    end
+  end
 end
