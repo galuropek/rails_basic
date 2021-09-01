caf94afcd75f59aad748d2732e47a591451df359
galuropek <Z322200119i>
2021-09-01 17:22:54 +0300

    add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year
+    Date.today.year
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,12 @@
 module QuestionsHelper
+  def question_header
+    case action_name
+    when "new"
+      "Create New #{@test.title} Question"
+    when "edit"
+      "Edit #{@question.test.title} Question"
+    else
+      "Question"
+    end
+  end
 end
