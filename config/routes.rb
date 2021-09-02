4a14ec4f3b165f29180568a7248dba3813f48be5
galuropek <Z322200119i>
2021-09-02 23:36:53 +0300

    move answers resources to questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :answers
   root 'tests#index'
 
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
   end
 end
