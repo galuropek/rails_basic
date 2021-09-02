d180393fbae88e8c8b1762d724213c486204a5b0
galuropek <Z322200119i>
2021-09-02 10:28:10 +0300

    fix question_header helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,12 +1,5 @@
 module QuestionsHelper
-  def question_header
-    case action_name
-    when "new"
-      "Create New #{@test.title} Question"
-    when "edit"
-      "Edit #{@question.test.title} Question"
-    else
-      "Question"
-    end
+  def question_header(question)
+    question.new_record? ? "Create New #{@test.title} Question" : "Edit #{@question.test.title} Question"
   end
 end
