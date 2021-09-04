a095b3740ff2f773f67e2b13edb1c2e5f2b21039
galuropek <Z322200119i>
2021-09-04 13:32:51 +0300

    update Question entity: controller, views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,6 @@ class QuestionsController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-  end
-
   def new
     @question = Question.new
   end
