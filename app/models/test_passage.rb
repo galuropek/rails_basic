8a2e5dce4ea73e0bd267acb842434438b81dd59f
galuropek <Z322200119i>
2021-09-04 13:55:05 +0300

    set next_question to test_passage in model callback

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,13 +4,10 @@ class TestPassage < ActiveRecord::Base
   belongs_to :current_question, class_name: 'Question'
 
   before_validation :before_validation_set_test_question, on: :create
+  before_update :before_save_set_next_question, on: :update
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
-
-    self.current_question = next_question
+    self.correct_questions += 1 if correct_answer?(answer_ids)
     save!
   end
 
@@ -24,15 +21,15 @@ class TestPassage < ActiveRecord::Base
     self.current_question = test.questions.first
   end
 
+  def before_save_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
   def correct_answer?(answer_ids)
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct_answers
-  end
-
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
