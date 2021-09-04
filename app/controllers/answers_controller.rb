8b4c7598b8f4a70863c41bd532ff0c8c308d5219
galuropek <Z322200119i>
2021-09-05 00:43:48 +0300

    answers_controller: fix incorrect methods after scaffolding

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,57 +1,51 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
+  before_action :set_question, only: [:new, :create]
 
   def show
   end
 
   def new
-    @answer = Answer.new
+    @answer = @question.answers.new
   end
 
   def edit
   end
 
   def create
-    @answer = Answer.new(answer_params)
+    answer = @question.answers.new(answer_params)
 
-    respond_to do |format|
-      if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
-        format.json { render :show, status: :created, location: @answer }
-      else
-        format.html { render :new }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
+    if answer.save
+      redirect_to question_path(@question)
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @answer }
-      else
-        format.html { render :edit }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
+    if @answer.update(answer_params)
+      redirect_to question_path(@answer.question)
+    else
+      render :edit
     end
   end
 
   def destroy
     @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to question_path(@answer.question)
   end
 
   private
 
-    def set_answer
-      @answer = Answer.find(params[:id])
-    end
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
 
-    def answer_params
-      params.require(:answer).permit(:body, :correct)
-    end
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body, :correct)
+  end
 end
