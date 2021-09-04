0fbb1097fbcc0082150c3a074d27ae6995d795ab
galuropek <Z322200119i>
2021-09-04 13:32:28 +0300

    update Answer entity: controller, views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,28 +1,16 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
 
-  # GET /answers
-  # GET /answers.json
-  def index
-    @answers = Answer.all
-  end
-
-  # GET /answers/1
-  # GET /answers/1.json
   def show
   end
 
-  # GET /answers/new
   def new
     @answer = Answer.new
   end
 
-  # GET /answers/1/edit
   def edit
   end
 
-  # POST /answers
-  # POST /answers.json
   def create
     @answer = Answer.new(answer_params)
 
@@ -37,8 +25,6 @@ class AnswersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /answers/1
-  # PATCH/PUT /answers/1.json
   def update
     respond_to do |format|
       if @answer.update(answer_params)
@@ -51,8 +37,6 @@ class AnswersController < ApplicationController
     end
   end
 
-  # DELETE /answers/1
-  # DELETE /answers/1.json
   def destroy
     @answer.destroy
     respond_to do |format|
@@ -62,12 +46,11 @@ class AnswersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_answer
       @answer = Answer.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def answer_params
       params.require(:answer).permit(:body, :correct)
     end
