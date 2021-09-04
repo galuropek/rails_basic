9478aa62263840a6cc85815d99693886cef528c6
galuropek <Z322200119i>
2021-09-04 13:35:12 +0300

    add test_passage method to User model and add "content" class to tag in application.html.erb for footer position

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true
 
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
+
   def tests_by_level(level)
     self.tests.where(level: level)
   end
