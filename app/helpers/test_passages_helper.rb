25235e2e816b2de05e709f3febb5b21c2cc5ec17
galuropek <Z322200119i>
2021-09-04 17:26:13 +0300

    add question number/total to test_passages show view, add question_number helper

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -6,4 +6,8 @@ module TestPassagesHelper
   def result_value(test_passage, tag_class)
     content_tag :span, test_passage.result_percentage, class: tag_class
   end
+
+  def question_number(test_passage)
+    test_passage.test.questions.order(:id).index(test_passage.current_question).next
+  end
 end
