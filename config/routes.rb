3e972e8072651adc9ff1170bf680950b03b9c5f0
galuropek <Z322200119i>
2021-09-07 23:35:13 +0300

    update routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
   root 'tests#index'
+
+  get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
+
+  resources :users, only: :create
+  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
