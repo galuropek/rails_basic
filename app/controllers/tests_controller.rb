ad48704d90f095c21163233f6e3b141e925f2213
galuropek <Z322200119i>
2021-09-08 21:07:49 +0300

    fix

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,6 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_test, only: %i[show edit update destroy, start]
+  before_action :set_test, only: %i[show edit update destroy start]
   before_action :set_user, only: :start
 
   def index
