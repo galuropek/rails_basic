12519d25ee8ef7c47b1fc787c27480a96389f951
galuropek <Z322200119i>
2021-09-08 21:07:27 +0300

    add flash_alert helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Verify your Email and/or Password please.'
       render :new
     end
   end
