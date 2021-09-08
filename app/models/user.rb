6d753b419837ff33f9f89f8c9ec2a60513fd4562
galuropek <Z322200119i>
2021-09-08 21:35:25 +0300

    add email format and uniqueness checking before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: :user_id
 
-  validates :email, presence: true
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+
+  before_save :downcase_email
 
   has_secure_password
 
@@ -14,5 +18,11 @@ class User < ActiveRecord::Base
 
   def tests_by_level(level)
     self.tests.where(level: level)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210903182031) do
+ActiveRecord::Schema.define(version: 20210908183243) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body",                        null: false
@@ -65,5 +65,7 @@ ActiveRecord::Schema.define(version: 20210903182031) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
