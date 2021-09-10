ae808e1b0ead66e1f6a61a02cd81cbe846674ffc
galuropek <Z322200119i>
2021-09-11 00:17:34 +0300

    remove actions from User`s TestsController

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,41 +1,9 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_test, only: :start
 
   def index
     @tests = Test.all
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def create
-    @test = Test.new(test_params)
-    if @test.save
-      redirect_to test_path(@test)
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @test.update(test_params)
-      redirect_to test_path(@test)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -44,10 +12,6 @@ class TestsController < ApplicationController
   end
 
   private
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id)
-  end
 
   def set_test
     @test = Test.find(params[:id])
