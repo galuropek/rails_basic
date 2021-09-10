30eec108d9d0ea56d03cbae8db5d980492167362
galuropek <Z322200119i>
2021-09-11 02:06:08 +0300

    add first and last name and flash message after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
+    flash[:notice] = "Привет, #{resource.first_name.present? ? resource.first_name : resource.email}!"
     resource.is_a?(Admin) ? admin_tests_path : tests_path
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:email, :first_name, :last_name, :password) }
   end
 end
