ac7806db9b19aa1374ce76eeb46ffa5e59a2ece9
galuropek <Z322200119i>
2021-09-11 01:10:40 +0300

    create a test through current_user

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -17,7 +17,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def create
-    @test = Test.new(test_params)
+    @test = current_user.created_tests.new(test_params)
     if @test.save
       redirect_to admin_test_path(@test)
     else
