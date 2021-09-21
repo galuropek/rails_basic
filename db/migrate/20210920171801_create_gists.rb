58fdb7a161ce8e447dfdb6a300dde7b2dd7951f6
galuropek <Z322200119i>
2021-09-21 11:22:56 +0300

    remove index from references

--- db/migrate/20210920171801_create_gists.rb
+++ db/migrate/20210920171801_create_gists.rb
@@ -2,8 +2,8 @@ class CreateGists < ActiveRecord::Migration
   def change
     create_table :gists do |t|
       t.string :url, null: false
-      t.references :question, index: true, foreign_key: true
-      t.references :user, index: true, foreign_key: true
+      t.references :question, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps null: false
     end
