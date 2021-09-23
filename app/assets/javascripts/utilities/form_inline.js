83758f656130ce339bc092695ff4e741fc2e1bc0
galuropek <Z322200119i>
2021-09-23 22:44:24 +0300

    add inline-form for title edit

---
+++ app/assets/javascripts/utilities/form_inline.js
@@ -0,0 +1,39 @@
+document.addEventListener('turbolinks:load', function () {
+  var controls = document.querySelectorAll('.form-inline-link')
+
+  if (controls.length) {
+    for (let i = 0; i < controls.length; i++) {
+      controls[i].addEventListener('click', formInlineClickHandler)
+    }
+  }
+
+  var errors = document.querySelector('.resource-errors')
+
+  if (errors) {
+    var resourceId = errors.dataset.resourceId
+    formInlineHandler(resourceId)
+  }
+})
+
+function formInlineClickHandler(event) {
+  event.preventDefault()
+
+  var testId = this.dataset.testId
+  formInlineHandler(testId)
+}
+
+function formInlineHandler(testId) {
+  var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
+  var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
+  var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+
+  if (formInline.classList.contains('hide')){
+    testTitle.classList.add('hide')
+    formInline.classList.remove('hide')
+    link.textContent = 'Cancel'
+  } else {
+    testTitle.classList.remove('hide')
+    formInline.classList.add('hide')
+    link.textContent = 'Edit'
+  }
+}
--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,20 +1,17 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :set_test, only: %i[show edit update destroy start update_inline]
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def new
     @test = Test.new
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @test = current_user.created_tests.new(test_params)
@@ -33,6 +30,14 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
     redirect_to tests_path
@@ -42,6 +47,10 @@ class Admin::TestsController < Admin::BaseController
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def set_tests
+    @tests = Test.all
   end
 
   def set_test
