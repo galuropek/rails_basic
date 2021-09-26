c2a1716f6ecb16152ebefa8048f5f353c0625563
galuropek <Z322200119i>
2021-09-27 00:48:07 +0300

    fix: smtp status=500

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,10 +63,8 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Action Mailer configuration
   # config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'test-guru-galuropek.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -74,7 +72,7 @@ Rails.application.configure do
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    authentication: 'plain',
+    authentication: :plain,
     enable_starttls_auto: true
   }
 
