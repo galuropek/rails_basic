160334e3e1710f8e5507029984ccd6185bd7ee87
galuropek <Z322200119i>
2021-09-29 11:38:19 +0300

    fix test_passage when test without questions

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,17 +1,25 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tests, only: %i[index start]
   before_action :set_test, only: :start
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def start
-    current_user.tests.push(@test)
-    redirect_to current_user.test_passage(@test)
+    if @test.questions.any?
+      current_user.tests.push(@test)
+      redirect_to current_user.test_passage(@test)
+    else
+      flash.now[:danger] = t('.no_questions', title: @test.title)
+      render :index
+    end
   end
 
   private
+
+  def set_tests
+    @tests = Test.all
+  end
 
   def set_test
     @test = Test.find(params[:id])
